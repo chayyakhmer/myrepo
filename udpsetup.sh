e0b5bc8d42eb1c349eb27902ade805d16bb650ae
chayyakhmer <109713546+chayyakhmer@users.noreply.github.com>
2023-01-31 15:11:51 +0700

    Update udpsetup.sh

--- udpsetup.sh
+++ udpsetup.sh
@@ -3,7 +3,7 @@ rm /data/data/com.termux/files/home/runudp.sh
 git clone https://github.com/chayyakhmer/udp.git && chmod +x /data/data/com.termux/files/home/udp/udp
 #chmod +x /data/data/com.termux/files/home/udp/udp
 #cp /data/data/com.termux/files/home/udp/runudp.sh /data/data/com.termux/files/home/runudp.sh
-rm runudp.sh 
+rm /data/data/com.termux/files/home/runudp.sh
 wget https://raw.githubusercontent.com/chayyakhmer/myrepo/main/runudp.sh && chmod +x /data/data/com.termux/files/home/runudp.sh
 
 #chmod +x /data/data/com.termux/files/home/runudp.sh
